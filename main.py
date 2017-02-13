8f58776833a1bc1d95d3735cc2bd8a0209725b38
Caid Buchman <caibuch2010@gmail.com>
2017-02-13 00:01:03 -0600

    She done

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import webapp2
 import jinja2
 import os
+import time
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
+
 
 class BlogPost(db.Model):
     title = db.StringProperty(required = True)
@@ -45,9 +47,19 @@ class MainBlog(Handler):
         self.render_front()
 
 class ViewPostHandler(Handler):
+    def render_front(self, title="", blog="", error=""):
+        self.render("grabbedblog.html", title=title, blog=blog, error=error)
+
+
     def get(self, id):
-        BlogPost.get_by_id(id)
-        self.response.write("i have {0}".format(id))
+        grabbedblog = BlogPost.get_by_id(int(id))
+        if not grabbedblog:
+            self.response.write("That's not an existing blog id")
+        else:
+            title = grabbedblog.title
+            blog = grabbedblog.blog
+            self.render("grabbedblog.html", title=title, blog=blog, grabbedblog=grabbedblog)
+
 
 class NewPost(Handler):
     def render_front(self, title="", blog="", error=""):
@@ -65,7 +77,11 @@ class NewPost(Handler):
         if title and blog:
             a = BlogPost(title = title, blog = blog)
             a.put()
-            self.redirect("/")
+            aid = str(a.key().id())
+            #self.response.write(aid)
+            time.sleep(1.0)
+            #self.redirect('/')
+            self.redirect('/blog/'+aid)
         else:
             error = "we need both a title and some story brah!"
             self.render("new-post.html", title=title, blog=blog, error=error)
